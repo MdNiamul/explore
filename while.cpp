385843ddf954f2a0b1d64a2e89a9d5d654c9f2e7
Md Niamul Islam <108489887+MdNiamul@users.noreply.github.com>
2026-02-09 22:47:37 +0600

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+string text;
+cout<<"Enter the text: "; 
+cin>>text;
+
+
+
+int i =1;
+while(i<=10)
+{
+    cout<<i<<" : "<<text<<endl;
+    i++;
+}
+
+cout<<"Aree you agree with me :";
+
+    getch();
+}
